c2b564b41af1951db011869b81cd6388d32de25c
keshavmehra79 <mehrakeshav332@gmail.com>
2025-11-12 16:32:01 +0530

    iamback

--- src/buyticket.jsx
+++ src/buyticket.jsx
@@ -1,32 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios"
 const Buyticket=()=>{
+    const[formdata,setdata]=useState({
+        category:"",
+        selecttime:"",
+        moviename:"",
+        date:"",
+        people:"",
+        price:""
+
+    })
+    const handlechange=(e)=>{
+           setdata({...formdata,[e.target.name]:e.target.value})
+           
+    }
+
+    const senddata=(e)=>{
+        e.preventDefault();
+        axios.post(`http://localhost:3000/ticketdata`,formdata).then(()=>{
+
+            alert("Data sent succefully........")
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+    }
 
     return(
         <>
         <div id="ticketbody">
             <div id="ticketform">
                 <h1>Book Your Tickets</h1>
-                <form action="">
+                <form onSubmit={senddata}>
                 Category<br></br>
-                <select name="" id="" placeholder="Select Category">
+                <select name="category" id="" value={formdata.category} onChange={handlechange}>
                     <option >Adult</option>
                     <option >Child</option>
                     <option >Senior</option>
                 </select><br></br>
-                Show Timing<br></br>
-                <select name="" id="" value="Select Time">
-                    <option value="">8:00am to 10:00am</option>
-                    <option value="">12:00am to 03:00am</option>
-                    <option value="">8:00pm to 10:00pm</option>
+                Select Time<br></br>
+                <select name="selecttime" id="" value={formdata.selecttime} onChange={handlechange}>
+                    <option value="08am:00-10:00am">08am:00-10:00am</option>
+                    <option value="12pm:00-03:00pm">12pm:00-03:00pm</option>
+                    <option value="08:00pm-10:00pm">08:00pm-10:00pm</option>
                 </select><br></br>
-                Name<br></br>
-                <input type="text" placeholder="Enter name" /><br></br>
+               Movie Name<br></br>
+                <input type="text" name="moviename" value={formdata.moviename}  onChange={handlechange} placeholder="Enter movie name" /><br></br>
                 Booking Date <br></br>
-                <input type="date" placeholder="dd-mm-yyyy"/><br></br>
+                <input type="date" name="date" value={formdata.date} placeholder="dd-mm-yyyy"  onChange={handlechange}/><br></br>
 
                 People<br></br>
-                <input type="text" placeholder="Enter number of people" />
+                <input type="text" name="people" value={formdata.people} placeholder="Enter number of people"  onChange={handlechange} />
 <br></br><br></br>
                 <button type="submit">Book Now </button>
                 </form>
--- src/tickets.jsx
+++ src/tickets.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Tickets=()=>{
+    const[mydata,setdata]=useState([])
+    const getdata=()=>{
+          axios.get(`http://localhost:3000/ticketdata`).then((res)=>{
+            setdata(res.data)
+            alert("Data fetched ...........")
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+    }
+
+
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/ticketdata`).then((res)=>{
+            setdata(res.data)
+            alert("Data fetched ...........")
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+    }
+   ,[])
+
+const del=(id)=>{
+    axios.delete(`http://localhost:3000/ticketdata/${id}`).then(()=>{
+        alert("Deleted")
+        getdata()
+}).catch((err)=>{
+    console.log(err)
+})
+}    
     return(
         <>
-        <h1>I will Show Status of tickets</h1>
-        
+        <table >
+            <thead><tr>
+                 <th>Id</th>
+                <th>Movie</th>
+                <th>Date</th>
+                <th>Price</th>
+                <th>People</th>
+                <th>Total</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+               
+            </thead>
+            <tbody>
+                {
+                    mydata.map((e)=>(
+                        <tr>
+                            <td>{e.id}</td>
+                            <td>{e.moviename}</td>
+                            <td>{e.date}</td>
+                            <td>{e.price}</td>
+                            <td>{e.people}</td>
+                            <td>{e.people*e.price}</td>
+                            <td>Edit</td>
+                            <td onClick={()=>{del(e.id)}}>Delete</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+            </table>        
         </>
     )
 }
